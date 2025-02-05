80219ec5f733d7bb3c480d23b5189215ef31cddf
Renan Gemi Guerra <renangemi@icloud.com>
2025-02-05 14:01:21 -0300

    Commit do projeto FilmScore: Adicionando Projeto Console

---
+++ FilmScore.Console/Program.cs
@@ -0,0 +1,58 @@
+﻿using FilmScore.Menus;
+using FilmScore.Modelos.Modelos;
+using FilmScore.Shared.Data.Banco;
+
+
+var context = new FilmScoreContext();
+var filmeDAL = new DAL<Filme>(context);
+
+Dictionary<int, Menu> opcoes = new();
+opcoes.Add(1, new MenuRegistrarFilme());
+opcoes.Add(2, new MenuExibirDetalhesFilme());
+opcoes.Add(3, new MenuFilmesRegistrados());
+opcoes.Add(4, new MenuExibirFilmePorAno());
+opcoes.Add(5, new MenuExibirFilmePorGenero());
+opcoes.Add(0, new MenuSair());
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+███████╗██╗██╗░░░░░███╗░░░███╗░██████╗░█████╗░░█████╗░██████╗░███████╗
+██╔════╝██║██║░░░░░████╗░████║██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝
+█████╗░░██║██║░░░░░██╔████╔██║╚█████╗░██║░░╚═╝██║░░██║██████╔╝█████╗░░
+██╔══╝░░██║██║░░░░░██║╚██╔╝██║░╚═══██╗██║░░██╗██║░░██║██╔══██╗██╔══╝░░
+██║░░░░░██║███████╗██║░╚═╝░██║██████╔╝╚█████╔╝╚█████╔╝██║░░██║███████╗
+╚═╝░░░░░╚═╝╚══════╝╚═╝░░░░░╚═╝╚═════╝░░╚════╝░░╚════╝░╚═╝░░╚═╝╚══════╝
+");
+    Console.WriteLine("             *****Bem Vindo ao FilmeScore*****            ");
+
+}
+
+void ExibirMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite [1] para adicionar um filme");
+    Console.WriteLine("Digite [2] para mostrar detalhes de um filme ");
+    Console.WriteLine("Digite [3] para mostrar todos filmes ");
+    Console.WriteLine("Digite [4] para mostrar todos filmes por ano");
+    Console.WriteLine("Digite [5] para mostrar todos filmes por gênero");
+    Console.WriteLine("Digite [0] para sair");
+    Console.Write("\nDigite a opção: ");
+
+    string opçãoEscolhida = Console.ReadLine()!;
+    int opçãoNumerica = int.Parse(opçãoEscolhida);
+
+    if (opcoes.ContainsKey(opçãoNumerica))
+    {
+        Menu menuASerExibido = opcoes[opçãoNumerica];
+        menuASerExibido.Executar(filmeDAL);
+        if (opçãoNumerica > 0) ExibirMenu();
+    }
+    else
+    {
+        Console.WriteLine ("Opção Inválida !");
+    }
+}
+
+
+ExibirMenu();
